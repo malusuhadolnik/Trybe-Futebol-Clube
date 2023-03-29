1ef22cf5fcd5371647da0ddb661354b4195e03f7
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-03-29 10:07:06 -0300

    escrita do model para a tabela teams

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING,} from 'sequelize'; // importamos os tipos direto do sequelize!
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: { // exemplo do Course: dia 02, model com sequelize
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false, // No readme não diz se pode ser null ou não, então setei para false para garantir que não vai ter erros no futuro
+  }
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
