651cec58a7bfd8004314fe56d1e2dbf7e7cef50b
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-03-30 09:33:23 -0300

    construção do controller de Teams

--- app/backend/src/database/services/teamsService.ts
+++ app/backend/src/database/services/teamsService.ts
@@ -3,27 +3,26 @@
 // além disso, vamos precisar de um constructor, que deve retornar a dependência dessa classe. Fonte: chat gpt
 // e no caso a dependência do service é a model
 
-// os método para listar todas as categorias e listar por ID com o sequelize 
-// foram usados no BlogsAPI, dar uma olhadinha lá no código. 
+// os método para listar todas as categorias e listar por ID com o sequelize
+// foram usados no BlogsAPI, dar uma olhadinha lá no código.
 // https://blog.logrocket.com/async-await-in-typescript/
 
-//lembrar de tipar a entrada e a saída 
+// lembrar de tipar a entrada e a saída
 // qual é o tipo do model?? https://sequelize.org/docs/v6/other-topics/typescript/#requesting-a-model-class
 // get all não tem entrada, mas tem saída: uma array de objetos representando dados de times
 // e essa array também será uma promise, já que o método é assíncrono.
-
-import Teams from "../models/Teams"
-import { ModelStatic } from 'sequelize'
+import { ModelStatic } from 'sequelize';
+import Teams from '../models/Teams';
 
 export default class TeamsService {
-  public _model: ModelStatic<any>
+  public _model: ModelStatic<Teams>; // a função do ModelStatic é avisar que o modelo em questão é estático
 
-  constructor(model: ModelStatic<any>) {
-    this._model = model
+  constructor(model: ModelStatic<Teams>) {
+    this._model = model;
   }
 
   public getAll = async (): Promise<Teams[]> => {
     const allTeams = await this._model.findAll();
     return allTeams;
-  }
+  };
 }
