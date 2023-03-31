97a56e4962532edb4b1e8d7fc0deddf32c69660d
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-03-31 17:40:01 -0300

    adição de uma função para verificação do JWT, de acordo com a mentoria estruturada

--- app/backend/src/database/utils/JWTFunctions.ts
+++ app/backend/src/database/utils/JWTFunctions.ts
@@ -10,3 +10,8 @@ const configJWT: Jwt.SignOptions = { algorithm: 'HS256', expiresIn: '48h',} // o
 export const createJWT = (payload: Omit<ILogin, 'password'>) => {
   return Jwt.sign(payload, JWT_SECRET, configJWT);
 }
+
+export const verifyJWT = (token: string) => {
+  console.log(token);
+  return Jwt.verify(token, JWT_SECRET);
+}
