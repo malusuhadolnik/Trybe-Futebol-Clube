00effba69ccde904b31319547bd49d478877d64a
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-04-07 19:31:07 -0300

    criação da query para buscar dados da leaderboard

--- app/backend/src/database/services/leaderboardService.ts
+++ app/backend/src/database/services/leaderboardService.ts
@@ -1,14 +1,15 @@
-// Obs: quando o método é estático, não precisamos de um construtor
+// Obs1: quando o método é estático, não precisamos de um construtor
+// Obs2: As queries serão deenvolvidas em outro arquivo por 2 motvos: facilidade de leitura,
+// e porque deu um erro avisando que havia parâmetros em excesso.
 // https://sequelize.org/docs/v6/core-concepts/raw-queries/
-import { QueryTypes } from "sequelize";
-import sequelize from '../models'
 
+import { QueryTypes } from 'sequelize';
+import sequelize from '../models';
+import { homeLBQuery } from '../utils/homeLBQuery';
 
-
-export default class LeaderboardService {  
-  static async getLeaderboardInfo() {
-    const [homeBoard] = await sequelize.query(homeLBQuery, { type: QueryTypes.SELECT })
+export default class LeaderboardService {
+  static async getHomeLeaderboard() {
+    const [homeBoard] = await sequelize.query(homeLBQuery, { type: QueryTypes.SELECT });
     return homeBoard;
   }
 }
-  
---
+++ app/backend/src/database/utils/homeLBQuery.ts
@@ -0,0 +1,62 @@
+// uma alternativa de resolução seria possível à partir da versão 8 do mql: as common table expressions
+// https://www.mysqltutorial.org/mysql-cte/
+// seria possível criar uma espécie de "tabela intermediária virtual e temporária",
+// para criar as colunas totalVictories, totalDraws e totalLosses, e a partir delas resolver os demais cálculos
+// na ausência desta funcionalidade, a única coisa que testei e funcinou
+// foi repetir o calculo em cada uma das colunas em que eles são necessários :(
+// porque não é possível referenciar colunas que ainda não foram criadas
+// Bibliografia consultada: Course, projeto one for All,
+// https://pt.stackoverflow.com/questions/543860/como-usar-case-when-sum-com-group-by
+
+export const homeLBQuery = `
+SELECT
+  teams.team_name AS name,
+  COUNT(matches.id) AS totalGames,
+  SUM(
+      CASE
+        WHEN matches.home_team_goals < matches.away_team_goals THEN 3
+        WHEN matches.home_team_goals = matches.away_team_goals THEN 1
+        ELSE 0
+        END
+    ) AS totalPoints,
+    SUM(
+        CASE
+          WHEN matches.away_team_id = teams.id
+          AND matches.away_team_goals > matches.home_team_goals THEN 1
+          ELSE 0
+        END
+    ) AS totalVictories,
+    SUM(
+        CASE
+          WHEN matches.away_team_id = teams.id
+          AND matches.away_team_goals = matches.home_team_goals THEN 1
+          ELSE 0
+        END
+    ) AS totalDraws,
+    SUM(
+        CASE
+          WHEN matches.away_team_id = teams.id
+          AND matches.away_team_goals < matches.home_team_goals THEN 1
+          ELSE 0
+        END
+    ) AS totalLosses,
+    SUM(matches.away_team_goals) AS goalsFavor,
+    SUM(matches.home_team_goals) AS goalsOwn,
+    SUM(matches.away_team_goals - matches.home_team_goals) AS goalsBalance,
+    FORMAT((SUM(
+      CASE
+        WHEN matches.away_team_id = teams.id
+        AND matches.away_team_goals > matches.home_team_goals THEN 3
+        WHEN matches.away_team_id = teams.id
+        AND matches.away_team_goals = matches.home_team_goals THEN 1
+        ELSE 0
+      END
+  ) / (COUNT(matches.id) * 3)) * 100, 2) AS efficiency
+FROM TRYBE_FUTEBOL_CLUBE.teams AS teams
+INNER JOIN TRYBE_FUTEBOL_CLUBE.matches AS matches
+ON teams.id = matches.home_team_id
+WHERE matches.in_progress = false
+GROUP BY teams.id;
+`;
+
+export const awayLBQuery = { status: 'em construção' };
