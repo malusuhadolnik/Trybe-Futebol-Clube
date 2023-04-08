65d427be80bcf6ddd7d48996d0f99e586c475473
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-04-08 11:18:28 -0300

    controller para o leaderboard de times away

--- app/backend/src/database/controllers/leaderboardController.ts
+++ app/backend/src/database/controllers/leaderboardController.ts
@@ -6,4 +6,9 @@ export default class LeaderboardController {
     const homeBoardData = await LeaderboardService.getHomeLeaderboard();
     res.status(200).json(homeBoardData);
   }
+
+  static async getAwayBoard(_req: Request, res: Response): Promise<void> {
+    const awayBoardData = await LeaderboardService.getAwayLeaderboard();
+    res.status(200).json(awayBoardData);
+  }
 }
