cb8de613ab84e889a2ce3035c5e668773c8af4c1
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2023-04-08 11:12:04 -0300

    criação da query para o leaderboard do time away

--- app/backend/src/database/utils/homeLBQuery.ts
+++ app/backend/src/database/utils/homeLBQuery.ts
@@ -57,6 +57,51 @@ GROUP BY teams.id
 ORDER BY totalPoints DESC, totalVictories DESC, goalsBalance DESC, goalsFavor DESC;
 `;
 
-export const awayLBQuery = { status: 'em construção' };
-
-// preciso colocar um orderby
+export const awayLBQuery = `
+SELECT
+    teams.team_name AS name,
+    COUNT(matches.id) AS totalGames,
+    SUM(
+      CASE
+        WHEN matches.away_team_goals > matches.home_team_goals THEN 3
+        WHEN matches.away_team_goals = matches.home_team_goals THEN 1
+        ELSE 0
+      END
+    ) AS totalPoints,
+    SUM(
+        CASE
+          WHEN matches.away_team_goals > matches.home_team_goals THEN 1
+          ELSE 0
+        END
+    ) AS totalVictories,
+    SUM(
+        CASE
+            WHEN matches.away_team_goals = matches.home_team_goals THEN 1
+            ELSE 0
+        END
+    ) AS totalDraws,
+    SUM(
+        CASE
+            WHEN matches.away_team_goals < matches.home_team_goals THEN 1
+            ELSE 0
+        END
+    ) AS totalLosses,
+    SUM(matches.away_team_goals) AS goalsFavor,
+    SUM(matches.home_team_goals) AS goalsOwn,
+    SUM(matches.away_team_goals - matches.home_team_goals) AS goalsBalance,
+    ROUND(
+        (SUM(
+            CASE
+                WHEN matches.away_team_goals > matches.home_team_goals THEN 3
+                WHEN matches.away_team_goals = matches.home_team_goals THEN 1
+                ELSE 0
+            END
+        ) / (COUNT(matches.id) * 3)) * 100, 2
+    ) AS efficiency
+FROM TRYBE_FUTEBOL_CLUBE.teams AS teams
+INNER JOIN TRYBE_FUTEBOL_CLUBE.matches AS matches
+ON teams.id = matches.home_team_id
+WHERE matches.in_progress = false
+GROUP BY teams.id
+ORDER BY totalPoints DESC, totalVictories DESC, goalsBalance DESC, goalsFavor DESC;
+`;
